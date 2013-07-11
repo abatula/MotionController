aa974c1933c3497fcf19740f2cba0382503ff2b8
Alyssa <batulaa@drexel.edu>
2013-07-11 14:54:23 -0400

    Added a main.cpp file for testing functionality

---
+++ main.cpp
@@ -0,0 +1,40 @@
+/* main.cpp
+ * Test the motion controller object
+ */
+
+#include <stdio.h>
+#include <string>
+#include <cstring>
+
+#include "motionController.h"
+
+
+int main(void)
+{  
+    MotionController *controller = new MotionController();
+    bool initialized = false;
+//    bool motionRunning = false;
+    
+//    initialized = controller->initMotionManager();
+//    usleep(1000000); // 1 second = 1,000,000 us
+    
+    if(initialized)
+    {
+        printf("\n Initialized");
+        /*
+        controller->executePage(60);
+        motionRunning = controller.actionRunning();
+        if(motionRunning)
+            printf("\n Motion is running");
+        else
+            printf("\n Motion ended");
+        usleep(5*1000000); // 1 second = 1,000,000 us
+        if(motionRunning)
+            printf("\n Motion is running");
+        else
+            printf("\n Motion ended");
+        */
+    }
+    else
+        printf("\n Initialization failed");
+}
