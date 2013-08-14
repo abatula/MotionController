a69ec08c6648b4b19254ae91a115cef25fea30a0
Alyssa Batula <batulaa@drexel.edu>
2013-08-14 15:31:51 -0400

    Updated functionality tests
    Detects that Darwin SHOULD be moving, but doesn't move and encounters a segfault in the first second or
    two after starting the motion.

--- main.cpp
+++ main.cpp
@@ -13,28 +13,44 @@ int main(void)
 {  
     MotionController *controller = new MotionController();
     bool initialized = false;
-//    bool motionRunning = false;
-    
-//    initialized = controller->initMotionManager();
-//    usleep(1000000); // 1 second = 1,000,000 us
+    bool motionRunning = false;
+    initialized = controller->initMotionManager();
+    usleep(1000000); // 1 second = 1,000,000 us
     
     if(initialized)
     {
-        printf("\n Initialized");
-        /*
+        printf("\n Initialized\n");
+        
         controller->executePage(60);
-        motionRunning = controller.actionRunning();
+        printf("\n Executed\n");
+        motionRunning = controller->actionRunning();
+        printf("\n Checked running\n");
         if(motionRunning)
-            printf("\n Motion is running");
+        {
+            printf("\n Motion is running\n");
+        }
         else
-            printf("\n Motion ended");
-        usleep(5*1000000); // 1 second = 1,000,000 us
-        if(motionRunning)
-            printf("\n Motion is running");
-        else
-            printf("\n Motion ended");
-        */
+        {
+            printf("\n Motion ended\n");
+        }
+        
+        while(motionRunning)
+        {
+            printf("\n Motion is running\n");
+            usleep(1000000);
+            motionRunning = controller->actionRunning();
+        }
+            
+//         printf("\n before sleep\n");
+//         usleep(5*1000000); // 1 second = 1,000,000 us
+//         printf("\n after sleep\n");
+//         motionRunning = controller->actionRunning();
+//         if(motionRunning)
+//             printf("\n Motion is running");
+//         else
+//             printf("\n Motion ended");
+        
     }
     else
-        printf("\n Initialization failed");
+        printf("\n Initialization failed\n");
 }
