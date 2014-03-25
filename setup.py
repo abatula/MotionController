89de62b4bb1f16bf2b95fed8d0f3acc6849afde6
Alyssa <batulaa@drexel.edu>
2014-03-25 16:12:40 -0400

    Added metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 
-setup(name='MotionController', 
+setup(name='MotionController',
+      version='0.0.1',
+      description='DARwIn-OP MotionController class',
+      author='Alyssa Batula',
+      author_email='batulaa@drexel.edu',
+      url='https://github.com/abatula/MotionController',
       ext_modules=[Extension('MotionController', ['MotionController.pyx', 'motionController.cpp'], 
                              language='c++',
                              extra_objects=['/darwin/Linux/lib/darwin.a'],
